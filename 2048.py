adab4d9f29da7a4d046d6a48656a6fa367144e24
Xinghao Zhou <zhou.xinghao.1991@gmail.com>
2016-06-28 22:16:59 -0400

    Add some comments

--- 2048.py
+++ 2048.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 
 actions = ['Up', 'Left', 'Down', 'Right', 'Restart', 'Exit']
 letter_codes = [ord(ch) for ch in 'WASDRQwasdrq']
+# {65: 'Left', 115: 'Down', 68: 'Right', 97: 'Left', 119: 'Up', 114: 'Restart',
+#  81: 'Exit', 82: 'Restart', 83: 'Down', 113: 'Exit', 87: 'Up', 100: 'Right'}
 actions_dict = dict(zip(letter_codes, actions * 2))
 
 
